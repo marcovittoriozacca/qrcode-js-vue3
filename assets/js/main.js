d418f8c70698f539e805b948f40c97427657e772
marcovittoriozacca <zaccamarco01@gmail.com>
2024-01-28 04:15:03 +0100

    fix: img was too big. feat: black and white qrcode + click on the img to download it

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,17 +9,24 @@ const { createApp } = Vue
     methods: {
         getUrlValue(){
           if(this.urlValue.length !== 0){
-
             const qrcode = new QRious({
                 element: document.getElementById("qrcode"),
                 value: this.urlValue,
-                background: 'hsl(216, 95%, 58%)',
+                background: '#FFFFFF',
                 backgroundAlpha: 1,
-                foreground: '#FFFFFF',
-                foregroundAlpha: 1,
-                size: 250,
-                
+                size: 200,
             });
+          }
+        },
+        download(){
+          if(this.urlValue.length !== 0){
+            const qrcodeCanvas = document.getElementById("qrcode");
+            image = qrcodeCanvas.toDataURL("image/png", 1.0).replace("image/png", "image/octet-stream");
+            const link = document.createElement('a');
+            link.download = 'qrcode.png';
+            link.href = image;
+            link.click();
+
           }
         }
     },
